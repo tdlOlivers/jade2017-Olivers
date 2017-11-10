e5504c95781a1669481e38dcd0d6fc833a83da02
Olivers Sipols <olivers.sipols@testdevlab.com>
2017-11-10 14:31:10 +0200

    updates

--- SearchTest.java
+++ SearchTest.java
@@ -10,5 +10,18 @@ public class SearchTest {
 		int x = 19;
 		assertEquals(Search.find(x, a), 4);
 	}
-
+	
+	@Test
+	public void testCase2() {
+		int[] a = {1, 3, 4, 5, 6, 8, 10, 12 ,13, 15, 19};
+		int x = 19;
+		assertEquals(Search.find(x, a), 10);
+	}
+	
+	@Test
+	public void testCase3() {
+		int[] a = {};
+		int x = 1;
+		assertEquals(Search.find(x, a), -1);
+	}
 }
