e09da016e44db4fefb1b4055227c46a83789f997
Olivers Sipols <olivers.sipols@testdevlab.com>
2017-11-10 11:08:57 +0200

    improved

--- Main.java
+++ Main.java
@@ -6,21 +6,21 @@ public class Main {
     System.out.println("Enter N:");
     Scanner scanner = new Scanner(System.in);
     String N = scanner.nextLine();
-    System.out.println(N);  
     int[] arr = Fibonaci(Integer.parseInt(N));  
     System.out.println(Arrays.toString(arr));
   }
 
   public static int[] Fibonaci(int index){
-    int[] arr = new int[index];
+    if (index < 0) {return null;}    
+    int[] arr = new int[index + 1];
     arr[0] = 0;
+    if (index == 0) {return arr;}    
     arr[1] = 1;
-    for(int i=2; i<index; i++){
+    if (index == 1) {return arr;}
+    for(int i=2; i<index+1; i++){
       arr[i] = arr[i-1] + arr[i-2];
     }
     return arr;
   }
 }
-
-
 
